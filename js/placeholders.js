4db003baa46da7a903f2d00ad245835c33be5447
nexisn4 <nexisn4@gmail.com>
2021-08-08 19:07:46 -0500

    removed head placeholder and added it back to each page. the favicon wasn't working with the other method.

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,5 +1,4 @@
 $(function(){
-    $("#head-placeholder").load("/templates/head.html");
     $("#nav-placeholder").load("/templates/nav.html");
     $("#footer-placeholder").load("/templates/footer.html");
 });
