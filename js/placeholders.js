91af0b2a55ffc53a745dbf5482d94cc4a1356434
nexisn4 <nexisn4@gmail.com>
2023-04-13 13:33:38 -0500

    ue update with bootstrap
    new nav, go-to-top button, restructure pages also

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,4 +1,31 @@
-$(function(){
-    $("#nav-placeholder").load("/templates/nav.html");
+$( document ).ready(function() {
+    // load nav
+    $("#nav-placeholder").load("/templates/nav.html", function(){
+        $(document).click(function (event) {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        // $('#toTopBtn').fadeOut();
+        $('#toTopBtn').css("display", "none");
+        
+        // load scrollup button function
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 50) {
+            $('#toTopBtn').fadeIn();
+          } else {
+            $('#toTopBtn').fadeOut();
+          }
+        });
+      
+        $('#toTopBtn').click(function() {
+          $("html, body").animate({
+            scrollTop: 0
+          }, 500);
+          return false;
+        });
+    });
+
+    // load footer
     $("#footer-placeholder").load("/templates/footer.html");
+
 });
